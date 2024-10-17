4a0738f61fd221ff3836da50f2bbce8f0c3c4a5b
Albert Domotor <hello@albertdomotor.ninja>
2024-10-17 21:36:40 +0300

    Modified table editor

--- app/src/lib/editor/component-editor/TableEditor.svelte
+++ app/src/lib/editor/component-editor/TableEditor.svelte
@@ -40,15 +40,39 @@
         columnLengths[paramName] = newLength;
     };
 
-    const handleInput = (elemName: string, paramName: string, e: Event) => {
+    let isError: null | {param: string, elem: string} = null;
+
+    let editedCell: {elem: string, param: string, value: string} | null = null;
+    const handleFocus = (elemName: string, paramName: string) => {
+        editedCell = {
+            elem: elemName,
+            param: paramName,
+            value: elements.find(element => element.name === elemName)?.[paramName] || ""
+        };
+    };
+
+    const handleInput = (e: Event) => {
         const paramVal = (e.target as HTMLInputElement).value;
 
         currentComponentJSON.update(val => {
             val.json.elements = val.json.elements.map(element => {
-                if (element.name === elemName) {
+                if (element.name === editedCell?.elem) {
+                    // milyen checkek legyenek?
+                    // type check (es eleve parseolja)
+                    // ha uresen van hagyva, torolje a key-t
+                    // nevet csekkolja hogy egyedi-e
+                    if (editedCell?.param === "name") {
+                        const elemNames = val.json.elements.map(e => e.name);
+                        if (elemNames.includes(paramVal)) {
+                            console.log("Name already exists");
+                            isError = {param: editedCell.param, elem: editedCell.elem};
+                            return element;
+                        }
+                    }
+
                     element = {
                         ...element,
-                        [paramName]: paramVal
+                        [editedCell.param]: paramVal
                     };
                 }
                 return element;
@@ -57,7 +81,7 @@
         });
 
         // update columnLengths
-        updateColLengths(paramName);
+        updateColLengths(editedCell?.param || '');
     };
 
     currentComponentJSON.subscribe(value => {
@@ -68,32 +92,86 @@
         }); 
     });
 
-    // handle pan
-    let tableOuter: HTMLDivElement;
-    let pointerdown = false;
-    const handlePan = (e: MouseEvent) => {
-        if (pointerdown) {
-            console.log(e.movementX);
-            tableOuter.scrollTop -= e.movementY;
-            tableOuter.scrollLeft -= e.movementX;
-        }
-    };
 
     // handle add element button
     let addElementButton: HTMLButtonElement;
     let isAddLayoverOpen = false;
 
-    onMount(() => {
-        document.addEventListener("pointerdown", () => {
-            pointerdown = true;
+    // handle pan
+    let tableOuter: HTMLDivElement;
+    let pointerdown = false;
+    const handlePan = (e: MouseEvent) => {
+        if (pointerdown) {
+            tableOuter.scrollTop -= e.movementY;
+            tableOuter.scrollLeft -= e.movementX;
+            isAddLayoverOpen = false;
+        }
+    };
+
+    // handle action buttons
+    let hoveredElement: string | null = null;
+
+    const handleElementDelete = (elemName: string) => {
+        currentComponentJSON.update(val => {
+            val.json.elements = val.json.elements.filter(element => element.name !== elemName);
+            return val;
         });
-        document.addEventListener("pointerup", () => {
-            pointerdown = false;
+    };
+
+    const moveUp = (elemName: string) => {
+        currentComponentJSON.update(val => {
+            let elements = [...val.json.elements];
+            const index = elements.findIndex(element => element.name === elemName);
+            if (index === 0) return val;
+
+            const elem = elements[index];
+            elements[index] = elements[index - 1];
+            elements[index - 1] = elem;
+
+            return {
+                ...val,
+                json: {
+                    ...val.json,
+                    elements
+                }
+            };
+        });
+    };
+
+    const moveDown = (elemName: string) => {
+        currentComponentJSON.update(val => {
+            let elements = [...val.json.elements];
+            const index = elements.findIndex(element => element.name === elemName);
+            if (index === elements.length - 1) return val;
+
+            const elem = elements[index];
+            elements[index] = elements[index + 1];
+            elements[index + 1] = elem;
+
+            return {
+                ...val,
+                json: {
+                    ...val.json,
+                    elements
+                }
+            };
+        });
+    };
+
+    onMount(() => {
+        document.addEventListener("pointerdown", (e) => {
+            pointerdown = true;
         });
         document.addEventListener("click", (e) => {
             if (e.target !== addElementButton) {
                 isAddLayoverOpen = false;
             }
+        });
+        document.addEventListener("dragstart", (e) => {
+            pointerdown = false;
+        });
+        document.addEventListener("pointerup", () => {
+            pointerdown = false;
         });
         document.addEventListener("keydown", (e) => {
             if (e.key === "Escape") {
@@ -107,20 +185,26 @@
 <div class="table-outer"
     bind:this={tableOuter}>
     <table class="main-table-editor"
+        on:mouseleave={() => hoveredElement = null}
         on:pointermove={handlePan}>
-        <tr>
+        <tr on:mouseenter={() => hoveredElement = null}>
             {#each columns as column}
                 <th>{column}</th>
             {/each}
         </tr>
         {#each elements as element}
-            <tr>
+            <tr on:mouseenter={() => hoveredElement = element.name}
+                class={hoveredElement === element.name ? 'hover' : ''}>
                 <td class="nodeNo">{nodeNos[element.name]}</td>
                 <td>
                     <input
                         type="text" value={element['name']}
-                        style={`width: ${columnLengths['name']*9.5}px;`}
-                        on:input={(e) => handleInput(element.name, 'name', e)}
+                        style={`width: ${columnLengths['name']*9.5 + 15}px;`}
+                        class="{(isError &&
+                            isError.param === 'name' &&
+                            isError.elem === element.name ? 'name-error' : '')}"
+                        on:focus={() => handleFocus(element.name, 'name')}
+                        on:input={handleInput}
                     />
                 </td>
                 <td class="no-edit">{element.type}</td>
@@ -132,8 +216,11 @@
                                     paramTypes[column] === "number" ? "number" : "text"
                                 }
                                 value={element[column] ? element[column] : ""}
-                                style={`width: ${columnLengths[column]*9.5}px;`}
-                                on:input={(e) => handleInput(element.name, column, e)}
+                                style={`width: ${
+                                    (columnLengths[column]*9 + (paramTypes[column] === "number" ? 15 : 0))
+                                    }px;`}
+                                on:focus={() => handleFocus(element.name, column)}
+                                on:input={handleInput}
                             />
                         </td>
                     {:else}
@@ -141,9 +228,31 @@
                         </td>
                     {/if}
                 {/each}
+                <div class="action-cont">
+                    <div class="action-inner">
+                        <button class="btn-action"
+                            on:click={() => moveUp(element.name)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
+                            </svg>                                                   
+                        </button>
+                        <button class="btn-action"
+                            on:click={() => moveDown(element.name)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+                            </svg>                                                  
+                        </button>
+                        <button class="btn-action rm"
+                            on:click={() => handleElementDelete(element.name)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                            </svg>                      
+                        </button>
+                    </div>
+                </div>
             </tr>
         {/each}
-        <tr class="row-add">
+        <tr class="row-add" on:mouseenter={() => hoveredElement = null}>
             <td class="cell-add" colspan="8">
                 <button class="add-inside"
                     bind:this={addElementButton}
@@ -167,6 +276,62 @@
     </table>
 </div>
 <style>
+    .action-cont {
+        position: absolute;
+        right: 0;
+        width: 100%;
+        pointer-events: none;
+    }
+    .action-inner {
+        width: fit-content;
+        position: sticky;
+        left: calc(100vw - 470px);
+        pointer-events: all;
+        margin-top: 4px;
+        display: flex;
+    }
+    .btn-action {
+        background: white;
+        border: none;
+        border-top: var(--main-border);
+        border-bottom: var(--main-border);
+        border-radius: 0;
+        width: 28px;
+        height: 28px;
+        cursor: pointer;
+        visibility: hidden;
+        margin: 0;
+    }
+    .btn-action:first-of-type {
+        border-left: var(--main-border);
+        border-top-left-radius: var(--main-border-radius);
+        border-bottom-left-radius: var(--main-border-radius);
+    }
+    .btn-action:last-of-type {
+        border-right: var(--main-border);
+        border-top-right-radius: var(--main-border-radius);
+        border-bottom-right-radius: var(--main-border-radius);
+    }
+    tr.hover .btn-action {
+        visibility: visible;
+    }
+    .btn-action svg {
+        width: 14px;
+        height: 14px;
+        margin-bottom: -3px;
+        margin-left: -1px;
+        color: rgba(0, 0, 0, 0.7);
+    }
+    .btn-action.rm svg {
+        color: var(--main-error-color-dark);
+    }
+    .btn-remove:hover {
+        background-color: var(--main-hover-color);
+    }
+    .name-error:focus {
+        outline: solid 2px var(--main-error-color);
+        border-radius: 2px;
+    }
     .add-inside {
         width: fit-content;
         position: relative;
@@ -265,7 +430,6 @@
     tr:first-of-type {
         position: sticky;
         top: 0;
-        background-color: white;
     }
     th:first-of-type {
         position: relative;
@@ -295,11 +459,16 @@
         border: none;
         font-family: 'Roboto Mono', monospace;
         font-size: 14px;
-        background-color: rgb(247, 247, 247);
+        background-color: rgba(0, 0, 0, 0.03);
     }
     .no-edit {
-        background-color: white;
         white-space: nowrap;
         padding: 8px;
+    }
+    tr {
+        background-color: white;
+    }
+    tr.hover {
+        background-color: rgb(250, 250, 250);
     }
 </style>
