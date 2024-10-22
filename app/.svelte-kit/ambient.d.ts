f4c2f86df3fdfc7713e8ff1ac888f304c34a7468
Albert Domotor <hello@albertdomotor.ninja>
2024-10-22 17:09:29 +0300

    added meta tags

--- app/.svelte-kit/ambient.d.ts
+++ app/.svelte-kit/ambient.d.ts
@@ -36,7 +36,7 @@ declare module '$env/static/private' {
 	export const ComSpec: string;
 	export const DriverData: string;
 	export const EDITOR: string;
-	export const EFC_13500: string;
+	export const EFC_17028: string;
 	export const FNM_ARCH: string;
 	export const FNM_COREPACK_ENABLED: string;
 	export const FNM_DIR: string;
@@ -144,7 +144,7 @@ declare module '$env/dynamic/private' {
 		ComSpec: string;
 		DriverData: string;
 		EDITOR: string;
-		EFC_13500: string;
+		EFC_17028: string;
 		FNM_ARCH: string;
 		FNM_COREPACK_ENABLED: string;
 		FNM_DIR: string;
